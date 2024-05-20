17a7ca34cde1839b6e1eabe60e72e28149c4f6a7
Olaf Schindler <studio@orkana39.pl>
2024-05-20 21:04:29 +0200

    Plugin Slugify

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    slugify: {
+        enabled: true,
+        config: {
+            shouldUpdateSlug: true,
+            contentTypes: {
+                restaurant: {
+                  field: 'Slug',
+                  references: 'Name',
+                },
+            },
+        }
+    }
+});
